138d9eaf764d1d1dde1abecdc4a327028b4246a2
David Mitchell <mitchellda@hhsc.ca>
2024-05-03 12:22:14 -0400

    Cleaned up code and started implimentation of FHIR retreival

--- RecursiveSummary.py
+++ RecursiveSummary.py
@@ -5,11 +5,22 @@ from os.path import isfile, join
 import csv
 import pathlib
 from openai import OpenAI
+from fhirclient import client
+from fhirclient.models import patient
 
 client = OpenAI(
 api_key=""
 )
-#summaries = [,]  [summary level, summary number]
+
+
+settings = {
+    'app_id': 'my_web_app',
+    'api_base': 'https://fhir-open-api-dstu2.smarthealthit.org',
+}
+smart = client.FHIRClient(settings=settings)
+patient = patient.Patient.read('hca-pat-1', smart.server)
+print(patient.birthDate.isostring)
+print(patient.name[0])
 
 def loadTextData():
     path = pathlib.Path(__file__).parent.resolve() / "TXTData"
@@ -30,25 +41,25 @@ def loadTextData():
 
 def summarize_data(data):
 
-        prompt = (f"You are an expert medical professional data analyst who is analyzing the data below. "
+        prompt = (f"You are an expert medical data analyst who is analyzing the data below. "
                   f"Please format your response as follows:\n"
-                  f"Summary: put your summary of the included data here\n"
                   f"Categories: include a list of categories that apply to the data below\n"
-                  f"Anomalies: Note any particular anomalies in the data\n"
-                  f"Insights: Please note any patterns or insights that can be gleaned from this data\n\n"
+                  f"Summary: put your summary of the included data here\n"
+                  f"Insights: Please note any patterns or insights that can be gleaned from this data which are not immediately obvious and not included in the above summary\n\n"
+                  f"Anomalies: Note any particular anomalies in the data here\n"
                   f"{data}")
-        response = client.chat.completions.create(model="gpt-3.5-turbo",messages=[{"role": "system", "content": prompt},])
+        response = client.chat.completions.create(model="gpt-4-turbo",messages=[{"role": "system", "content": prompt},])
         return response.choices[0].message.content
 
 def meta_summary(data):
-        prompt = (f"You are an expert medical professional data analyst who is analyzing the data below. This is a collection of summaries you previous generated. Please summarize these summaries"
+        prompt = (f"You are an expert medical  data analyst who is analyzing the data below. This is a collection of summaries you previous generated. Please summarize these summaries"
                   f"Please format your response as follows:\n"
                   f"Summary: put your summary of the included data here\n"
                   f"Categories: include a list of categories that apply to the data below\n"
                   f"Anomalies: Note any particular anomalies in the data\n"
                   f"Insights: Please note any patterns or insights that can be gleaned from this data\n\n"
                   f"{data}")
-        response = client.chat.completions.create(model="gpt-3.5-turbo",messages=[{"role": "system", "content": prompt},])
+        response = client.chat.completions.create(model="gpt-4-turbo",messages=[{"role": "system", "content": prompt},])
         return response.choices[0].message.content
 
     
@@ -81,21 +92,16 @@ def main():
     chunk = 0
     summaries = []
     for i in splitDataResult:
-        print("Summary " + str(chunk))
+        print("\nSummary of chunk " + (str)(chunk + 1) + "\n")
+
         print(summarize_data(splitDataResult[chunk]))
         summaries.append(summarize_data(splitDataResult[chunk]))
         chunk+=1
 
     allsummaries = " ".join([str(item) for item in summaries])
 
-    print("Meta analysis")
+    print("\nMeta analysis\n")
     print(meta_summary(allsummaries))
-    summarizationLevel = 0
-    summaryNumber = 0
-    
-    CSV_directory_path = '/CSVData'
-    csv_data = loadTextData()
-    openai.api_key = "YOUR_OPENAI_API_KEY"
 
 if __name__ == "__main__":
     main()
